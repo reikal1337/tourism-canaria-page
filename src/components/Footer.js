4c55feffb151bf44bfb92dba0cb0b2c0f1aca98b
LAPTOP-5Q44CSU7\reika <reikal951@gmail.com>
2023-11-12 22:12:47 +0200

    Changed footer link.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
     return(
         <p className="footer--copyright">Copyright &copy; 2023-{(new Date().getFullYear())} All Rights Reserved by
-         <a className="footer--link" href="index.html"> Reika Developments</a></p>
+         <a className="footer--link" href="https://lukasreika.com/" target="_blank" rel="noreferrer"> Reika Developments</a></p>
     )
 }
 
